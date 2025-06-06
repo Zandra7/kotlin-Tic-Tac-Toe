4f6b23b96c3fb5c04d8b0291ca06fcb9225b119f
Zandra7 <sandra.eng@live.no>
2025-06-06 09:29:42 +0200

    Steg 2 - tar imot input fra bruker og printer det ut
    Co-authored-by: Lucas Bateson <lucas.william.bateson@nav.no>

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,9 +3,13 @@ package org.example
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
+    val c = readln()
+    // val c: CharArray = input.toCharArray()
     print("""
-        X O X
-        O X O
-        X X O 
+        ---------
+        | ${c[0]} ${c[1]} ${c[2]} |
+        | ${c[3]} ${c[4]} ${c[5]} |
+        | ${c[6]} ${c[7]} ${c[8]} |
+        ---------
     """.trimIndent())
 }
